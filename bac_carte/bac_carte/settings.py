7ae709ef0345d51a4eec58be3bbb0253439c4711
Lois Postula <lois.postula@gmail.com>
2017-03-21 17:34:10 +0100

    [enh] try again to log

--- bac_carte/bac_carte/settings.py
+++ bac_carte/bac_carte/settings.py
@@ -116,7 +116,59 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 DEBUG_LOG_DIR = "/var/log/app-logs/django_debug.log"
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    # How to format the output
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    # Log handlers (where to go)
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'log_file': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': DEBUG_LOG_DIR,
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    # Loggers (where does the log come from)
+    'loggers': {
+        'repackager': {
+            'handlers': ['console', 'log_file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['console', 'log_file'],
+            'level': 'WARN',
+            'propagate': False,
+        },
+        '': {
+            'handlers': ['console', 'log_file'],
+            'level': 'DEBUG',
+        },
+    }
+}
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 

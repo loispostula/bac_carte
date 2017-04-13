5d0849443ee88c0e54a557e5e21a9feb08d2fac6
Loïs Postula <lois.postula@gmail.com>
2017-04-13 09:40:16 +0200

    freeking python3.4

--- bac_carte/settings.py
+++ bac_carte/settings.py
@@ -137,6 +137,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
